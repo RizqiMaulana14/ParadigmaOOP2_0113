45a8a6259d322d4d5c8993eefab09b6dcd6d3984
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-03 17:23:18 +0700

    Pembuatan Base Class Seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,2 +1,10 @@
 #include<iostream>
 using namespace std;
+
+class seseorang{
+    public:
+        //virutal void pesam() = 0;
+        virtual void pesan(){
+            cout << "PEsan Dari Seseorang" << endl;
+        }
+};
