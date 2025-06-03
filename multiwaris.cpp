641144e82153b5f16a9b938c82ce05c2f1f4614a
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-03 18:24:25 +0700

    Pembuatan Multi Waris

---
+++ multiwaris.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class orang{
+    public:
+        int umur;
+        orang(int pUmur) :
+            umur(pUmur){
+                cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+            }
+};
+
+class pekerja{
+    public:
+        pekerja (int pUmur) :
+            orang(pUmur){
+                cout << "Pekerja dibuat\n" << endl;
+            }
+};
