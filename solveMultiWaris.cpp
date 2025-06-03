dfd7743a295348d55049f324e8958dfea2719850
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-03 18:40:56 +0700

    Pembuatan Program Utama

--- solveMultiWaris.cpp
+++ solveMultiWaris.cpp
@@ -32,7 +32,13 @@ class budi: public pekerja, public pelajar{
     public:
         budi(int pUmur) :
             pekerja(pUmur),
-            pelajar(pUmur),{
+            pelajar(pUmur){
                 cout << "Budi dibuat\n" << endl;
             }
 };
+
+int main(){
+    budi a(12);
+
+    return 0;
+}
