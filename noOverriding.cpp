dcec885e6be6460ccb64a803ed7b4b5b5c23e069
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-03 18:49:44 +0700

    Pembuatan Final

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 class baseClass{
     public:
-        virtual void perkenalan(){
+        virtual void perkenalan() final {
             cout << "Hallo ,saya function dari base class";
         }
 };
 
 class derivedClass : public baseClass{
     public:
-        void perkenalan(){
+        void perkenalan1(){
             cout << "Hallo, saya Function dari derived class";
         }
 };
