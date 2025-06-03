52fa6214977cb4ba1dcd980a6a1a4ef93bf617d7
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-03 17:39:31 +0700

    Pembuatan Program Utama

--- abstarksi.cpp
+++ abstarksi.cpp
@@ -18,3 +18,11 @@ class AbstraksiKlas{
             cout << "Y = " << y << endl;
         }
 };
+
+int main(){
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
